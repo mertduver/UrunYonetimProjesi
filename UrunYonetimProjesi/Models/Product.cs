32c343b7d1eb6ef13eb6e44af91d03b116cbe056
MERT DÜVER <mmert1505@gmail.com>
2021-09-23 20:22:33 +0300

    Add project files.

---
+++ UrunYonetimProjesi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UrunYonetimProjesi.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public DateTime ProductExpireDate { get; set; }
+        public decimal ProductFee { get; set; }
+        public string CurrencyName { get; set; }
+
+    }
+}
